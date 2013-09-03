13dd2907eb41861999b4e59a4a67e3f9bae8f6aa
Minal Test <minalfulzele@gmail.com>
2013-09-03 08:26:14 +0100

    Test: createAddressBookEntry_getFirstNameOfFirstEntry_firstNameOFFirstEntry()/FFailed

---
+++ AddressBook/src/com/gumtree/addressbook/AddressBookEntry.java
@@ -0,0 +1,36 @@
+package com.gumtree.addressbook;
+
+import java.util.Date;
+
+public class AddressBookEntry 
+{
+	private String firstName;
+	private String lastName;
+	private String sex;
+	private Date dob;
+
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+}
