a9a2c08558e6f041e1e49517cfabb8f5a5588901
Minal Test <minalfulzele@gmail.com>
2013-09-03 08:07:57 +0100

    Test : readAddressBook_getSecondLine_SecondlineinTextFormat/Passed

--- AddressBook/src/com/gumtree/addressbook/AddressBook.java
+++ AddressBook/src/com/gumtree/addressbook/AddressBook.java
@@ -1,7 +1,10 @@
 package com.gumtree.addressbook;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class AddressBook 
 {
@@ -9,7 +12,32 @@ public class AddressBook
 	
 	  public List<String> getAddressBook() 
 	  {
-		  addressBook.add("Bill McKnight, Male, 16/03/77");
-	    return addressBook;
+		  this.scanAddressBookFile();
+		  return addressBook;
+	  }
+	  
+	  private  final void scanAddressBookFile()
+	  {
+		  final File file;
+		  String fileLocation = "D:\\Workspace\\Gumtree\\AddressBook\\Resources\\AddressBook";
+
+		  file = new File(fileLocation);
+		  Scanner scanner = null;
+		  try
+		  {
+			  scanner = new Scanner(file);
+			  while(scanner.hasNextLine())
+			  {
+				  addressBook.add(scanner.nextLine());
+			  }
+		  }
+		  catch(final FileNotFoundException ex)
+		  {
+			  ex.printStackTrace();
+		  }
+		  finally
+		  {
+			  scanner.close();
+		  }
 	  }
 }
